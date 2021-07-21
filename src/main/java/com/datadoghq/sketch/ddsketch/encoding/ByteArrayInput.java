544eb3a8f6a94101b14182985243e33c3e31a00f
Charles Masson <charles.masson@datadoghq.com>
2021-07-21 16:21:50 +0200

    Use Unsafe to read longs

--- src/main/java/com/datadoghq/sketch/ddsketch/encoding/ByteArrayInput.java
+++ src/main/java/com/datadoghq/sketch/ddsketch/encoding/ByteArrayInput.java
@@ -7,14 +7,30 @@ package com.datadoghq.sketch.ddsketch.encoding;
 
 import java.io.EOFException;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.ByteOrder;
 import java.util.Objects;
+import sun.misc.Unsafe;
 
 /** An implementation of {@link Input} that is backed by an array. */
-public final class ByteArrayInput implements Input {
+public class ByteArrayInput implements Input {
 
-  private final byte[] array;
-  private final int endPos;
-  private int pos;
+  private static final Unsafe UNSAFE;
+
+  static {
+    Unsafe unsafe = null;
+    try {
+      Field f = Unsafe.class.getDeclaredField("theUnsafe");
+      f.setAccessible(true);
+      unsafe = (Unsafe) f.get(null);
+    } catch (IllegalAccessException | NoSuchFieldException ignored) {
+    }
+    UNSAFE = unsafe;
+  }
+
+  final byte[] array;
+  final int endPos;
+  int pos;
 
   private ByteArrayInput(byte[] array, int offset, int length) {
     Objects.requireNonNull(array);
@@ -27,7 +43,14 @@ public final class ByteArrayInput implements Input {
   }
 
   public static ByteArrayInput wrap(byte[] array, int offset, int length) {
-    return new ByteArrayInput(array, offset, length);
+    final ByteOrder nativeOrder = ByteOrder.nativeOrder();
+    if (ByteOrder.LITTLE_ENDIAN.equals(nativeOrder)) {
+      return new LittleEndianNativeOrderByteArrayInput(array, offset, length);
+    } else if (ByteOrder.BIG_ENDIAN.equals(nativeOrder)) {
+      return new BigEndianNativeOrderByteArrayInput(array, offset, length);
+    } else {
+      return new ByteArrayInput(array, offset, length);
+    }
   }
 
   public static ByteArrayInput wrap(byte[] array) {
@@ -48,7 +71,7 @@ public final class ByteArrayInput implements Input {
   }
 
   @Override
-  public final long readLongLE() throws IOException {
+  public long readLongLE() throws IOException {
     if (pos > endPos - 8) {
       throw new EOFException();
     }
@@ -63,5 +86,45 @@ public final class ByteArrayInput implements Input {
     value |= Byte.toUnsignedLong(array[pos + 7]) << 56;
     pos += 8;
     return value;
+  }
+
+  private static class BigEndianNativeOrderByteArrayInput extends ByteArrayInput {
+
+    public BigEndianNativeOrderByteArrayInput(byte[] array, int offset, int length) {
+      super(array, offset, length);
+    }
+
+    @Override
+    public long readLongLE() throws IOException {
+      if (pos > endPos - 8) {
+        throw new EOFException();
+      }
+      final long value =
+          Long.reverseBytes(
+              UNSAFE.getLong(
+                  array,
+                  (long) pos * Unsafe.ARRAY_BYTE_INDEX_SCALE + Unsafe.ARRAY_BYTE_BASE_OFFSET));
+      pos += 8;
+      return value;
+    }
+  }
+
+  private static class LittleEndianNativeOrderByteArrayInput extends ByteArrayInput {
+
+    private LittleEndianNativeOrderByteArrayInput(byte[] array, int offset, int length) {
+      super(array, offset, length);
+    }
+
+    @Override
+    public long readLongLE() throws IOException {
+      if (pos > endPos - 8) {
+        throw new EOFException();
+      }
+      final long value =
+          UNSAFE.getLong(
+              array, (long) pos * Unsafe.ARRAY_BYTE_INDEX_SCALE + Unsafe.ARRAY_BYTE_BASE_OFFSET);
+      pos += 8;
+      return value;
+    }
   }
 }
